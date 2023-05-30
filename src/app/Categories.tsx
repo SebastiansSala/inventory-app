2bd935184083afb0f40916e9164f5a97406ea67a
Sebastian Sala <sebastian_orko@hotmail.com>
2023-05-30 14:56:46 -0600

    database working

--- src/app/Categories.tsx
+++ src/app/Categories.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 import CategoriesList, { CategoriesProps } from "./CategoriesList";
-import { BsMotherboard, BsCpu, BsMemory, BsGpuCard } from "react-icons/bs"
-import { LuPcCase } from "react-icons/lu"
-import { ImPowerCord } from "react-icons/im"
+import { BsMotherboard, BsCpu, BsMemory, BsGpuCard } from "react-icons/bs";
+import { LuPcCase } from "react-icons/lu";
+import { ImPowerCord } from "react-icons/im";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
 
-export default function Categories() {
+const Categories: NextPage = () => {
+  const router = useRouter();
+
+  const handleClick = (title: string) => {
+    router.push(`/categories/${title}`);
+  };
+
   const categoriesItems: CategoriesProps[] = [
-    { title: "CPUs", icon: <BsCpu /> },
-    { title: "Memory", icon: <BsMemory />  },
-    { title: "Motherboards", icon: <BsMotherboard />  },
-    { title: "GPUs", icon: <BsGpuCard />  },
-    { title: "Computer Cases", icon: <LuPcCase />  },
-    { title: "Power Supplies", icon: <ImPowerCord />  },
+    { title: "CPUs", icon: <BsCpu />, handleClick },
+    { title: "Memory", icon: <BsMemory />, handleClick },
+    { title: "Motherboards", icon: <BsMotherboard />, handleClick },
+    { title: "GPUs", icon: <BsGpuCard />, handleClick },
+    { title: "Computer Cases", icon: <LuPcCase />, handleClick },
+    { title: "Power Supplies", icon: <ImPowerCord />, handleClick },
   ];
 
   return (
     <main className="container mx-auto border">
       <section className="grid grid-cols-3 gap-4 flex-wrap p-7">
         {categoriesItems.map((item, index) => (
-          <CategoriesList key={index} title={item.title} icon={item.icon}/>
+          <CategoriesList
+            key={index}
+            title={item.title}
+            icon={item.icon}
+            handleClick={handleClick}
+          />
         ))}
       </section>
     </main>
   );
-}
+};
+
+export default Categories;
--- src/app/CategoriesList.tsx
+++ src/app/CategoriesList.tsx
@@ -1,16 +1,23 @@
-import React from "react"
+import React from "react";
 
-export default function CategoriesList({title, icon}:CategoriesProps ){
-    
-    return(
-        <div className="flex flex-col justify-center items-center hover:cursor-pointer hover:shadow-md place-content-center border transition-all duration-300 p-2">
-            <span className="text-8xl">{icon}</span>
-            <span>{title}</span>
-        </div>
-    )
+export default function CategoriesList({
+  title,
+  icon,
+  handleClick,
+}: CategoriesProps) {
+  return (
+    <div
+      className="flex flex-col justify-center items-center hover:cursor-pointer hover:shadow-md place-content-center border transition-all duration-300 p-2"
+      onClick={(e) => handleClick(title)}
+    >
+      <span className="text-8xl">{icon}</span>
+      <span>{title}</span>
+    </div>
+  );
 }
 
 export interface CategoriesProps {
-    title: string,
-    icon: React.ReactNode
+  title: string;
+  icon: React.ReactNode;
+  handleClick: (title: string) => void;
 }
